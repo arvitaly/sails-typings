6bc56a6171652bb9a3e52e9528b063a11ef7cd14
Alexandro <arvitaly@list.ru>
2016-11-21 16:26:00 +0700

    add websockets

--- index.d.ts
+++ index.d.ts
@@ -33,8 +33,23 @@ declare namespace Sails {
             routes: any;
         };
     }
+    export interface Request {
+
+    }
+    export type Id = string | number;
     export interface Model extends Waterline.Collection {
         globalId: string;
+        // WebSockets Resourceful PubSub http://sailsjs.org/documentation/reference/web-sockets/resourceful-pub-sub  
+        message(id: Id, data: any, req?: Request): Model;
+        publishAdd(id: Id, association: string, added: any, req?: Request, options?: { noReverse: boolean }): Model;
+        publishCreate(data: any, req?: Request): Model;
+        publishRemove(id: Id, association: string, fk: string, req?: Request, options?: { noReverse: boolean }): Model;
+        publishDestroy(id: Id, req?: Request, options?: { previous: any }): Model;
+        publishUpdate(id: Id, changes: any, req?: Request, options?: { noReverse: boolean }): Model;
+        unsubscribe(req: Request, ids: Array<Id>): Model;
+        watch(req: Request): Model;
+        subscribe(req: Request, ids: Array<Id>): Model;
+        unwatch(req: Request): Model;
     }
     export interface Module {
         constructor: new () => App;
